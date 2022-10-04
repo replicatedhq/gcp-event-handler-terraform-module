d4241c4bb538147c371df1bcd9ae0b17a96a8baf
Chase <chase@replicated.com>
2022-10-04 12:42:01 -0700

    updating source_dir to source_file

--- main.tf
+++ main.tf
@@ -74,6 +74,6 @@ resource "google_cloudfunctions_function" "handler_function" {
 
 data "archive_file" "handler_function_zip" {
   type        = "zip"
-  source_dir  = "${path.root}${var.handler_path}"
+  source_file = "${path.root}${var.handler_path}"
   output_path = "${path.module}/tmp/handler_function.zip"
 }
